959e32df7f6c8b9f638382ff5ca1108fad0e91ed
Javier Berastegui <javier.bg0391@gmail.com>
2022-07-09 00:52:25 +0200

    Creacion super lista

--- Python/diccionarios_listas_anidados.py
+++ Python/diccionarios_listas_anidados.py
@@ -2,7 +2,13 @@ def run():
     lista = [1, "Maria", False, 8.8]
     diccionario = {"Nombre": "Javier", "Apellido": "Berastegui"}
 
-    
+    super:lista = [
+        {"Nombre": "Javier", "Apellido": "Berastegui"}
+        {"Nombre": "Adrian", "Apellido": "Hernandez"}
+        {"Nombre": "Kevin", "Apellido": "de la Rosa"}
+        {"Nombre": "Asahel", "Apellido": "Garcia"}
+        {"Nombre": "Alejandro", "Apellido": "Lorenzo"}
+    ]
 
 
 if __name__ == '__main__':
