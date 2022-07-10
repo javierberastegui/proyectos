9fcba751af0685ac9446ebc52983cf62de6cec7b
Javier Berastegui <javier.bg0391@gmail.com>
2022-07-10 12:00:53 +0000

    Sin compresion_de_listas

--- Python/compresion_de_listas.py
+++ Python/compresion_de_listas.py
@@ -1,11 +1,13 @@
+# Sin comprension de listas 
+
 def run():
     cuagrados = []
-    for i in range(1, 250):
+    for i in range(1, 70):
         if i % 3 != 0:
             cuagrados.append(i**6)
 
     print(cuagrados)
 
 
-if __main__ == '__name__':
+if __name__ == '__main__':
     run()
