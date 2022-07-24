57f7364fe97fd49222abf77209383e580e1328d4
javierberastegui <108772231+javierberastegui@users.noreply.github.com>
2022-07-24 09:45:49 +0100

    Subiendo archivos declarar funciones y variables
    Profesionalizando nuestro código

---
+++ Python/declarando_funciones.py
@@ -0,0 +1,5 @@
+# Declarando funciones 
+def suma(a: int, b: int) -> int:
+	return a + b
+
+print(suma(5, 9))
---
+++ Python/declarar_variable.py
@@ -0,0 +1,10 @@
+# Declaremos variables 
+
+a: int = 5
+print(a)
+
+b: str = "Es un string"
+print(b)
+
+c: bool = False
+print(c)
