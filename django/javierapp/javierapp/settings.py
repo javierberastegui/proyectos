201785c9a7092314b245e3d595c92be370d9938b
Javier Berastegui <javier.bg0391@gmail.com>
2022-12-20 19:40:57 +0000

    COMENTARIO

--- django/javierapp/javierapp/settings.py
+++ django/javierapp/javierapp/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# HAY QUE MODIFICAR
 SECRET_KEY = 'django-insecure-7)5e=@!up@h+#89@&xrfd8$gkqazt-@!*7=pc5n1m91)6nryw='
 
 # SECURITY WARNING: don't run with debug turned on in production!
